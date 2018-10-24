82e88f021f924a1751aecfa94f0ab1fe206f48f7
jamescross91 <james_cross91@hotmail.com>
2018-10-24 21:39:08 +0100

    Bump API version

--- src/get_inventory.js
+++ src/get_inventory.js
@@ -28,7 +28,7 @@ exports.handler = (event, context, callback) => {
       body: JSON.stringify(
         {
           'Results': data.Items,
-          'API Version': "v3.0"
+          'API Version': "v4.0"
         }
       ),
     };
